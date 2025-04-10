99e427437f01d4b8f8125aebf6730b10a4315551
Eveieve <jhkdev1@gmail.com>
2025-04-10 12:18:50 +0900

    build: log42j의존성 추가

--- src/main/java/com/ssg/springex/sample/SampleService.java
+++ src/main/java/com/ssg/springex/sample/SampleService.java
@@ -4,16 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @ToString
 @Service
 @RequiredArgsConstructor  // 스프링3에서 권장하는 생성자 주입 방식으로 세팅하기. autowired쓰지 않고.
+@Log4j2
 public class SampleService {
 
-
-    //@Autowired
-    // 인터페이스라고 되어있지만 사실 구현첵를 가져오는 것임 (?)
+    @Qualifier("normal") // 이름 지정해주기
     private SampleDAO sampleDAO;
 }
